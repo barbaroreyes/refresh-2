c655ad59e2275fc9a2aa4f0748d492e36580ffd5
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-02-04 22:57:50 -0600

    update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -19,6 +19,24 @@ const DUMMY_DATA = [
     description:
       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
   },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
 ];
 
 const AllMeetups = () => {
@@ -30,7 +48,7 @@ const AllMeetups = () => {
          <div key={i} className="meetup">
            
            <div className="picture">
-           <h2>{item.title}</h2>
+           <h2 className='meet-up-name'>{item.title}</h2>
            <img src={item.image} alt={item}/>
            <p>{item.description}</p>
            </div>
