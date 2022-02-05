949d898cf97160041f5a113234140cec06204221
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-02-05 09:10:21 -0600

    added-useref

--- src/pages/NewMeetUpsFomr.js
+++ src/pages/NewMeetUpsFomr.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import Card from '../compo/ui/Card';
 
 const NewMeetuPForm = () => {
+ const titleInput = useRef()
+ const imageInput = useRef()
+ const addressInput = useRef()
+ const descriptionInput = useRef()
+ 
+  const submitHandler = (e) => {
+    e.preventDefault();
+ const enterredTitle = titleInput.current.value;
+  }
   return (
     <Card>
      <div>
      <h1>Add a new meetup</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="control">
          <label htmlFor='title'>Meetup  Title</label>
-         <input type='text' required id='title'/>
+         <input type='text' required id='title' ref={titleInput}/>
         </div>
         <div className="control">
          <label htmlFor='image'>Meetup  Image</label>
-         <input type='url' required id='image'/>
+         <input type='url' required id='image' ref={imageInput}/>
         </div>
         <div className="control">
          <label htmlFor ='address'>Meetup  Address</label>
-         <input type='text' required id='address'/>
+         <input type='text' required id='address' ref={addressInput}/>
         </div>
         <div className="control">
          <label htmlFor ='description'>Meetup  Description</label>
-         <textarea id='description' required rows='5'></textarea>
+         <textarea id='description' required rows='5' ref={descriptionInput}></textarea>
         </div>
         <div className="actions">
           <button type>Add Meetup</button>
